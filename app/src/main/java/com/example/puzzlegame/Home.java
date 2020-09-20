cd3b7e15660bf9e6f14bd252bdff12605b0f9743
vovaser1990 <vovasrg@gmail.com>
2020-09-20 14:21:55 -0700

    update

---
+++ app/src/main/java/com/example/puzzlegame/Home.java
@@ -0,0 +1,27 @@
+package com.example.puzzlegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Button startButton = findViewById(R.id.start_button);
+         startButton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent chooseDifficultIntent = new Intent(Home.this,ChooseDifficultActivity.class);
+                 startActivity(chooseDifficultIntent);
+             }
+         });
+
+
+    }
+}
